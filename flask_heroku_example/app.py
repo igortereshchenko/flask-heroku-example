72e538588ad73d455ce1601e18bb1c9b10f7feb8
igortereshchenko <tereshchenko.igor@gmail.com>
2019-03-23 21:00:32 +0200

    added new route /hello

--- flask_heroku_example/app.py
+++ flask_heroku_example/app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
     return render_template('index.html', names=['Bob','Boba','Boban'])
+
+
+@app.route('/hello<name>')
+def index(name):
+    return "Hello, {0}".format(name)
